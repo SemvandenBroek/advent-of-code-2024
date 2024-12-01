3416ef8839b3d152882cc90651030941a22dfed6
Sem van den Broek <sem.vandenbroek@momomedical.com>
2024-12-01 18:48:13 +0100

    feat: add day1 solution

---
+++ src/day1/solution.test.ts
@@ -0,0 +1,36 @@
+import {solution} from "./solution";
+import {expect} from "chai";
+import * as fs from "node:fs";
+
+describe('solution', () => {
+  const exampleInput = `
+      3   4
+      4   3
+      2   5
+      1   3
+      3   9
+      3   3
+    `;
+
+  it('should result in the correct totalDistance with exampleInput', () => {
+    const result = solution(exampleInput);
+    expect(result.totalDistance).to.equal(11);
+  });
+
+  it('should result in the correct similarity score with exampleInput', () => {
+    const result = solution(exampleInput);
+    expect(result.similarityScore).to.equal(31);
+  });
+
+  it('should result in the correct totalDistance with actual input', () => {
+    const actualInput = fs.readFileSync('./src/day1/input.txt', 'utf8');
+    const result = solution(actualInput);
+    expect(result.totalDistance).to.equal(1970720);
+  });
+
+  it('should result in the correct similarityScore with actual input', () => {
+    const actualInput = fs.readFileSync('./src/day1/input.txt', 'utf8');
+    const result = solution(actualInput);
+    expect(result.similarityScore).to.equal(17191599);
+  });
+})
---
+++ src/day1/solution.ts
@@ -0,0 +1,42 @@
+import _ from "lodash";
+
+export function solution(input: string) {
+  const lines = input.split('\n');
+
+  const leftList: number[] = [];
+  const rightList: number[] = [];
+  let totalDistance = 0;
+  let similarityScore = 0;
+
+  const numberCount = new Map<number, number>();
+
+  for (const line of lines) {
+    const match= line.match(/(\d+)\s+(\d+)/);
+    if (!match) continue;
+
+    const left = match[1];
+    const right = match[2];
+
+    leftList.push(parseInt(left, 10));
+    rightList.push(parseInt(right, 10));
+  }
+
+  leftList.sort();
+  rightList.sort();
+
+  for (const [left, right] of _.zip(leftList, rightList)) {
+    if (!left || !right) continue;
+
+    totalDistance += Math.abs(left - right);
+  }
+
+  for (const right of rightList) {
+    numberCount.set(right, (numberCount.get(right) ?? 0) + 1);
+  }
+
+  for (const left of leftList) {
+    similarityScore += left * (numberCount.get(left) ?? 0);
+  }
+
+  return {totalDistance, similarityScore};
+}
